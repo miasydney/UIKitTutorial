f8938051f9285b632f7ce1b71c80d13c17d35069
mia <miabenn74@gmail.com>
2024-06-23 22:59:19 +1000

    Add IBActions for label and button in Storyboard

--- UIKitTutorial/Basics/ViewController.swift
+++ UIKitTutorial/Basics/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var primaryButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemMint
+        
     }
-
-
+    
+    func greetUser(_ name: String) {
+        greetingLabel.text = "Hello, \(name)"
+    }
+    
+    @IBAction func primaryButtonTapped(_ sender: Any) {
+        print("DEBUG: Button tapped.")
+        greetUser("User name")
+        view.backgroundColor = .white
+    }
+    
+    
 }
 

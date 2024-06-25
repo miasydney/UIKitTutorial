b286b86be88437d01d246eee44ffd693b369f84f
mia <miabenn74@gmail.com>
2024-06-25 23:57:22 +1000

    Create a ScrollView in ScrollViewTutorialViewController

---
+++ UIKitTutorial/Basics/ScrollView/ScrollViewTutorialViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ScrollViewTutorialViewController.swift
+//  UIKitTutorial
+//
+//  Created by Mia on 25/6/2024.
+//
+
+import UIKit
+
+class ScrollViewTutorialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

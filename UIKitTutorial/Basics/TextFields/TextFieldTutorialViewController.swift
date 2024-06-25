f3f4e1c4bb0901aae3fb7e6109c20fbbe0818f50
mia <miabenn74@gmail.com>
2024-06-25 23:48:06 +1000

    Add TextFieldTutorialViewController extension with functions showing how we can access the value in a text field, as well as return on keyboard, didBeginEditing and didEndEditing

--- UIKitTutorial/Basics/TextFields/TextFieldTutorialViewController.swift
+++ UIKitTutorial/Basics/TextFields/TextFieldTutorialViewController.swift
@@ -13,7 +13,7 @@ class TextFieldTutorialViewController: UIViewController {
        let tf = UITextField()
         tf.placeholder = "Search..."
         tf.borderStyle = .roundedRect
-        tf.keyboardType = .numberPad
+        tf.keyboardType = .emailAddress
         tf.font = UIFont.systemFont(ofSize: 14)
         tf.returnKeyType = .search
        return tf
@@ -23,13 +23,36 @@ class TextFieldTutorialViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         
+        textField.delegate = self
+        textField.addTarget(self, action: #selector(onEditingChanged), for: .editingChanged)
+
         view.addSubview(textField)
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         textField.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         textField.widthAnchor.constraint(equalToConstant: view.frame.width - 64).isActive = true
     }
+    
+    @objc func onEditingChanged(_ sender: UITextField) {
+        print("Debug: Text field text is \(sender.text)")
+    }
 
+}
+
+extension TextFieldTutorialViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("DEBUG: Return key tapped...")
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("DEBUG: Did begin editing")
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("DEBUG: Did end editing")
+    }
 }
 
 #Preview {

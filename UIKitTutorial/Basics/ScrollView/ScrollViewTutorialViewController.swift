1f84b766887858f03ac962da5817eaa87da43feb
mia <miabenn74@gmail.com>
2024-06-26 00:00:03 +1000

    Create a ScrollView in ScrollViewTutorialViewController

--- UIKitTutorial/Basics/ScrollView/ScrollViewTutorialViewController.swift
+++ UIKitTutorial/Basics/ScrollView/ScrollViewTutorialViewController.swift
@@ -9,21 +9,54 @@ import UIKit
 
 class ScrollViewTutorialViewController: UIViewController {
 
+    private var scrollView = UIScrollView()
+    
+    private var stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.spacing = 16
+        return stack
+    }()
+    
+    private let rectHeight: CGFloat = 200
+    private let numberOfRects = 20
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for _ in 1 ..< numberOfRects {
+            let rect = createRectangle()
+            stackView.addArrangedSubview(rect)
+        }
+        
+        view.addSubview(scrollView)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // pin scrollview to edges of screen
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        scrollView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        scrollView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
 
-        // Do any additional setup after loading the view.
+        // add stack view to scrollview
+        scrollView.addSubview(stackView)
+        stackView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
+        
+        scrollView.contentSize.height = (rectHeight * CGFloat(numberOfRects)) + 64
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func createRectangle() -> UIView {
+        let view = UIView()
+        view.heightAnchor.constraint(equalToConstant: rectHeight).isActive = true
+        view.widthAnchor.constraint(equalToConstant: rectHeight).isActive = true
+        view.backgroundColor = .systemGray
+        view.layer.cornerRadius = 16
+        return view
     }
-    */
+    
+}
 
+#Preview {
+    ScrollViewTutorialViewController()
 }

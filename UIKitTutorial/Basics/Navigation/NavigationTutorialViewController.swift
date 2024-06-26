0872d984876e4ad6113261c8ffc468767b18d6e7
mia <miabenn74@gmail.com>
2024-06-26 22:18:23 +1000

    Add NavigationTutorialViewController class, add new view presenting/ navigating to it when button is tapped

---
+++ UIKitTutorial/Basics/Navigation/NavigationTutorialViewController.swift
@@ -0,0 +1,49 @@
+//
+//  NavigationTutorialViewController.swift
+//  UIKitTutorial
+//
+//  Created by Mia on 26/6/2024.
+//
+
+import UIKit
+
+class NavigationTutorialViewController: UIViewController {
+
+    private var primaryButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Click me", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // center button on screen
+        view.addSubview(primaryButton)
+        primaryButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        primaryButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        primaryButton.addTarget(self, action: #selector(onPrimaryButtonTap), for: .touchUpInside)
+        
+    }
+    
+    @objc func onPrimaryButtonTap() {
+        // navigate to another screen
+        let newVC = CollectionTutorialViewController()
+//        show(newVC, sender: self)
+        
+        if let nav = navigationController {
+            print("DEBUG: Navigating with nav controller")
+            nav.pushViewController(newVC, animated: true)
+        } else {
+            print("DEBUG: Presenting modal sheet")
+            newVC.modalPresentationStyle = .automatic
+            self.present(newVC, animated: true)
+        }
+        
+//        navigationController?.pushViewController(newVC, animated: true)
+        
+    }
+
+}
